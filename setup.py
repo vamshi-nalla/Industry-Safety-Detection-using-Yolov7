dfaadedcd6da4cc5509a0e24480bce1421dc99c7
Vamshi-nalla <nvkrishna9396@gmail.com>
2024-03-29 13:22:02 +0530

    template & setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Vamshi-nalla',
+    author_email= 'nvkrishna9396@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
